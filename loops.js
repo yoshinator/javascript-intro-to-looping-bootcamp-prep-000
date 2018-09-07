d98640276c1ff4de3dce0e2a7c0dfc6bc27fa988
yoan ante <yoanante@gmail.com>
2018-09-07 14:14:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(stuff){
-  for (var i =0; i < 25; i++){
+  for (var i = 0; i < 25; i++){
     if (i ===1){
     stuff.push('I am 1 strange loop.')
     }
@@ -7,5 +7,5 @@ function forLoop(stuff){
     stuff.push(`I am ${i} strange loops.`)
     }
   }
-  return array;
+  return stuff;
 }
